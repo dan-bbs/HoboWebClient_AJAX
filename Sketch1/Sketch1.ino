f67f4036c12015ce99ed5ccb1224f5efa0bbcf72
danstar <danial.boytsov@gmail.com>
2018-01-25 05:01:43 +0200

    hobobo

---
+++ Sketch1/Sketch1.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		Sketch1.ino
+ Created:	1/25/2018 1:59:15 AM
+ Author:	dna
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
